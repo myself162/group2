e46a1bcf2a573cc3265bfc091f1bfa9354c04b75
myself162 <myself162@hotmail.com>
2014-01-16 17:25:46 +0800

    ch1 commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,7 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @groups = Group.find(params[:id])
+    @group = Group.find(params[:id])
   end
 
   def new
@@ -16,6 +16,27 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.save
       redirect_to groups_path
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+
+    redirect_to groups_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Group2::Application.routes.draw do
   resources :groups
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => 'groups#index', :id => 'welcome'
 end
